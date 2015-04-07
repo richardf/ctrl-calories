521e9dfed3d26eaf85b3d1bf44c9067330da6f6b
richardf <richardfuch@gmail.com>
2015-04-07 12:21:53 -0300

    separation of concerns in view

---
+++ app/assets/javascripts/angular/controllers/HomeCtrl.js
@@ -0,0 +1,40 @@
+angular.module('CtrlCalories')
+    .controller('HomeCtrl', ['$rootScope', '$scope', '$location', 'Auth',
+        function ($rootScope, $scope, $location, Auth) {
+            // function successAuth(res) {
+            //     localStorage.setItem("token", res.auth_token);
+            //     window.location = "/";
+            // }
+
+            // $scope.signin = function (login, password) {
+            //     var formData = {
+            //         login: login,
+            //         password: password
+            //     };
+
+            //     Auth.signin(formData, successAuth, function () {
+            //         $rootScope.error = 'Invalid credentials.';
+            //     })
+            // };
+
+            // $scope.signup = function (login, name, password, exp_calories) {
+            //     var formData = {
+            //         login: login,
+            //         password: password,
+            //         name: name,
+            //         expected_calories: exp_calories
+            //     };
+
+            //     Auth.signup(formData, successAuth, function (res) {
+            //         $rootScope.error = res.error || 'Failed to sign up.';
+            //     })
+            // };
+
+            // $scope.logout = function () {
+            //     Auth.logout(function () {
+            //         window.location = "/"
+            //     });
+            // };
+            // $scope.token = localStorage.getItem("token");
+            // $scope.tokenClaims = Auth.getTokenClaims();
+        }]);
