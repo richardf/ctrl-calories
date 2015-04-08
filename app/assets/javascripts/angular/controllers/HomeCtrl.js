181a632094323bd12afd14c03a6e124ffd90a217
richardf <richardfuch@gmail.com>
2015-04-08 20:44:04 -0300

    initial home design

--- app/assets/javascripts/angular/controllers/HomeCtrl.js
+++ app/assets/javascripts/angular/controllers/HomeCtrl.js
@@ -2,5 +2,12 @@ angular.module('CtrlCalories')
     .controller('HomeCtrl', ['$rootScope', '$scope', '$location', 'Auth',
         function ($rootScope, $scope, $location, Auth) {
 
+        	$scope.clearFilter = function() {
+        		$scope.filterStartDt = null;
+        		$scope.filterStartTime = null;
+        		$scope.filterEndDt = null;
+        		$scope.filterEndTime = null;
+        	};
+
 
         }]);
