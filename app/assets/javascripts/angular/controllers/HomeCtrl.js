211d31f0ec9d5a4f8b3de559761b4c6fafd62c68
richardf <richardfuch@gmail.com>
2015-04-10 11:25:59 -0300

    creating meals

--- app/assets/javascripts/angular/controllers/HomeCtrl.js
+++ app/assets/javascripts/angular/controllers/HomeCtrl.js
@@ -16,11 +16,34 @@ angular.module('CtrlCalories')
         	$scope.deleteMeal = function() {
 	        	Meal.delete($scope.mealToDelete).success(function() {
 	        		$scope.mealToDelete = null;
+                    loadProfile();
 	        		loadMeals();
 	        	}).error(function(err) {
 	        		$scope.error = err.error;
 	        	});
         	};
+
+            $scope.createMeal = function(meal) {
+                Meal.create(meal).success(function() {
+                    loadProfile();
+                    loadMeals();
+                    $scope.newMeal = null;
+                }).error(function(err) {
+                    console.log(err);
+                    $scope.error = err.error;
+                });
+            };            
+
+            $scope.updateMeal = function(meal) {
+                Meal.update(id, meal).success(function() {
+                    loadProfile();
+                    loadMeals();
+                    $scope.updateMeal = null;
+                }).error(function(err) {
+                    console.log(err);
+                    $scope.error = err.error;
+                });
+            };
 
     		function loadProfile() {
 	        	Profile.get().success(function(data) {
